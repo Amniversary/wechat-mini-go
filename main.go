49b764101cc5d55788ed2b35a887b12c47847f80
Gavean <623006297@qq.com>
2018-04-16 21:15:01 +0800

    log

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"github.com/Amniversary/wechat-mini-go/server"
 	"github.com/Amniversary/wechat-mini-go/config"
+	"log"
 )
 
 func main() {
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
 	server.NewServer(config.NewConfig()).Run()
 }
